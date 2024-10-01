013c4d635434061d52749fccd5558db6544003d8
Surya Kant Mani <suryakant.mani@6sense.com>
2024-10-01 16:35:58 +0530

    Update script.js

--- Feedback_Extension/script.js
+++ Feedback_Extension/script.js
@@ -25,7 +25,7 @@ new Vue({
       feedback: ''
     },
     sheets: [],  // Array to hold sheet names
-    slackWebhookUrl: 'https://hooks.slack.com/services/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', // Replace with your Slack webhook URL
+    slackWebhookUrl: 'https://hooks.slack.com/services/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', // Replace with your Slack webhook URL
   },
   mounted() {
     this.initializeTableau();
